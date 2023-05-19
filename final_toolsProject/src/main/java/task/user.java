1ceb71851bee06f8a3a143f9d694526fe737dfbb
menna <mennadiaah14@gmail.com>
2023-05-19 04:32:58 +0300

    making initial user class

--- final_toolsProject/src/main/java/task/user.java
+++ final_toolsProject/src/main/java/task/user.java
@@ -1,5 +1,42 @@
 package task;
 
-public class user {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "users")
+public class user implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID") 
+	private long id;
+	private String name;
+	private String role;
+	
+	user()
+	{}	
+	void setName(String name)
+	{
+		this.name=name;
+	}
+	@Column(name="Name")
+	String getName()
+	{
+		return name;
+	}
+	void setRole(String role)
+	{
+		this.role=role;
+	}
+	@Column(name="Role")
+	String getRole()
+	{
+		return role;
+	}
 
 }
